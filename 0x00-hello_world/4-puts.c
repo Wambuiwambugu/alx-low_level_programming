8351bdda3401812167d55aeb086133167a9e95d3
wambuiwambugu <2wambugu.marie@gmail.com>
2022-04-07 15:51:29 -0700

    first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * function main - entry point
+ * main - Prints "\"Programming is like building a multilingual puzzle"
  *
- * Return 0 (success)
+ * return (0)
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-    return 0;
+	return (0);
 }
