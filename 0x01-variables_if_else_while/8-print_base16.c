bc599d7ce645f90e10d89767d09043ea7090c15c
wambuiwambugu <2wambugu.marie@gmail.com>
2022-04-11 09:14:02 -0700

    base 16 characters

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print base 16
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	int n;
+	char ch;
+
+	for (n = 48; n < 59; n++)
+	{
+		putchar(n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
