c6ce0048382e4b18e10647abb291556bf6e7e883
wambuiwambugu <2wambugu.marie@gmail.com>
2022-04-11 08:02:26 -0700

    uppercase aphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * DEscription: print alphabet in uppercase
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
