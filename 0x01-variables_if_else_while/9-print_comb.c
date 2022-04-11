5b2445fa394bbe2885b6b975407cb7a9af9329c4
wambuiwambugu <2wambugu.marie@gmail.com>
2022-04-11 09:29:28 -0700

    single digit combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print combinations of two digit number
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
