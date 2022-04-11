a330463d0ea01b65db71c315ba5a4a3f7cf1fb57
wambuiwambugu <2wambugu.marie@gmail.com>
2022-04-11 08:52:44 -0700

    print base 10 with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- entry point
+ *
+ * Description: print base 10
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
