2d83fb11e546da33eb03c13638bef184c24e0d6c
wambuiwambugu <2wambugu.marie@gmail.com>
2022-04-14 16:33:25 -0700

    fizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints 1 to 100, fizz for *3, buzz for *5, fizbuzz for *3 && *5
+ *
+ * Description: new linwe after 100
+ *
+ * Return: always (0)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf(" Fizz");
+		}
+		else if (i % 3 != 0 && i % 5 == 0)
+		{
+			printf(" Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
+		}
+		else
+		{
+			printf(" %d", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
