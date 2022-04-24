0dd5a3a5daa6e0bf0e390405b8fef505ba9945c4
wambuiwambugu <2wambugu.marie@gmail.com>
2022-04-24 11:59:13 -0700

    concatenate n bytes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *_strncat - concatenates two strings
+ * @dest: first string
+ * @src: second string
+ * @n: number of bytes from src
+ *
+ * Return: concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, k = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[k] != '\0' && n > k)
+	{
+		dest[i] = src[k];
+		k++;
+		i++;
+	}
+	if (n > 0)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
