f20698597382c0ab5bf2eed6597a2de02ae73df1
wambuiwambugu <2wambugu.marie@gmail.com>
2022-06-16 09:58:12 -0700

    locate substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int a;
+
+	while (*haystack != '\0')
+	{
+		a = 0;
+		while (*haystack == *needle && *haystack != '\0' && *needle != '\0')
+			haystack++, needle++, a++;
+		if (*needle == '\0')
+			return (haystack - a);
+		haystack -= (a - 1), needle -= a;
+	}
+	return ('\0');
+}
